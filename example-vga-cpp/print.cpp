e6aa4bfb541e88c8da3d802ac6979e467b4faad5
krusty <krusty@vxn.dev>
2025-06-30 02:44:48 +0200

    Implement example syscall function in C++

--- example-vga-cpp/print.cpp
+++ example-vga-cpp/print.cpp
@@ -1,12 +1,23 @@
 #define ADDR 0xb8000
 
+long syscall(long num, long a1, long a2, long a3) {
+    long ret;
+    asm volatile (
+        "int $0x7f"
+        : "=a"(ret)
+        : "a"(num), "D"(a1), "S"(a2), "d"(a3)
+        : "rcx", "r11", "memory"
+    );
+    return ret;
+}
+
 extern "C" int main()
 {
-    char *p = (char *)ADDR;
+    /*char *p = (char *)ADDR;
     *p = 'j';
     p = p + 2;
-    *p = 'a';
+    *p = 'a';*/
 
-    return 33;
+    return syscall(1, 0, 0, 0);
 }
 
