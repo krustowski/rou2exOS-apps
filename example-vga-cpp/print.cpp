a19f426cb7dd330cdd6dd694b639f810e9211434
krusty <krusty@vxn.dev>
2025-06-30 20:15:17 +0200

    Update cpp example

--- example-vga-cpp/print.cpp
+++ example-vga-cpp/print.cpp
@@ -1,23 +1,18 @@
 #define ADDR 0xb8000
 
-long syscall(long num, long a1, long a2, long a3) {
-    long ret;
+typedef long int64_t;
+
+extern "C" __attribute__((naked)) int64_t main()
+{
+    int64_t num = 1, len = 18, ret;
+    const char *s = "Hello from C++!\n\0";
+
     asm volatile (
         "int $0x7f"
         : "=a"(ret)
-        : "a"(num), "D"(a1), "S"(a2), "d"(a3)
+        : "a"(num), "D"((int64_t)s), "S"(len), "d"(0)
         : "rcx", "r11", "memory"
     );
     return ret;
-}
-
-extern "C" int main()
-{
-    /*char *p = (char *)ADDR;
-    *p = 'j';
-    p = p + 2;
-    *p = 'a';*/
-
-    return syscall(1, 0, 0, 0);
 }
 
