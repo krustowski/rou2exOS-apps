63f631ffdbcc754eb92287d4d09f415daa28d501
krusty <krusty@vxn.dev>
2025-06-30 23:37:30 +0200

    Update and fix C++ syscall implementation (wip)

--- cpp/example-print/print.cpp
+++ cpp/example-print/print.cpp
@@ -1,18 +1,26 @@
-#define ADDR 0xb8000
+#include "syscall.h"
 
-typedef long int64_t;
-
-extern "C" __attribute__((naked)) int64_t main()
+extern "C" int64_t syscall(int64_t number, int64_t arg1, int64_t arg2, int64_t arg3) 
 {
-    int64_t num = 1, len = 18, ret;
-    const char *s = "Hello from C++!\n\0";
-
+    int64_t ret;
     asm volatile (
         "int $0x7f"
         : "=a"(ret)
-        : "a"(num), "D"((int64_t)s), "S"(len), "d"(0)
+        : "a"(number), "D"(arg1), "S"(arg2), "d"(arg3)
         : "rcx", "r11", "memory"
     );
+    return ret;
+}
+
+extern "C" int64_t main()
+{
+    int64_t len = 0;
+    const char *s = "Hello from C++!\n";
+
+    while (s[len]) ++len;
+
+    int64_t ret = syscall(ScSysinfo, (int64_t)s, len, 0);
+
     return ret;
 }
 
