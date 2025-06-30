70c83086910115b8ec77619ee2829063a86821f6
krusty <krusty@vxn.dev>
2025-06-30 13:03:49 +0200

    Add experimental string print using ABI in Rust

--- example-vga-rs/src/lib.rs
+++ example-vga-rs/src/lib.rs
@@ -12,7 +12,10 @@ pub extern "C" fn entry(arg: u32) -> u32 {
         *vga.offset(1) = 0x1E30 + (arg as u16); // '0' + arg
     }*/
 
-    return syscall(2, arg as u64, 34) as u32
+    let s = b"Hello, interrupt!\n";
+    let len = s.len();
+
+    return syscall(1, s.as_ptr() as u64, len as u64) as u32
 }
 
 #[inline(always)]
@@ -32,7 +35,7 @@ pub fn syscall(n: u64, a1: u64, a2: u64) -> u64 {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    if let Some(location) = info.location() {
+    if let Some(_location) = info.location() {
         //string(vga_index, location.file().as_bytes(), Color::Red);
         //number(vga_index, location.line() as u64);
     } else {
